2316e5cd19adaa47a0085b6154724ebc7df8862d
Monika <laiskutis.monikai@gmail.com>
2024-09-04 11:44:58 +0300

    like my dog

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Card from './assets/Components/Card'
+
+function App() {
+  
+  return (
+    <>
+    <Card />
+
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/Components/Card.jsx
@@ -0,0 +1,41 @@
+import { BsEmojiSmile } from "react-icons/bs";
+import { FaHeart } from "react-icons/fa";
+import { useState } from "react";
+
+function Card () {
+
+    const [heart, setHeart] = useState(false)
+    const [count, setCount] = useState(0)
+
+    const handleHeart = () => {
+        if (!heart) {
+            setHeart(true)
+            setCount(count +1)
+        }
+
+        else { 
+            setHeart(false)
+            setCount(count -1)
+    }
+}
+
+    return (
+        <div className="card">
+            <span>Patiktukai: {count}</span>
+            <div className="card card-header" style={{display: "flex", justifyContent: "center", alignItems: "center", gap: 30}}>
+                <BsEmojiSmile />
+                <p style={{marginTop:15}}>Please like my Dog</p>
+            </div>
+
+        <img src="https://i.pinimg.com/originals/e5/b9/81/e5b98110fcd62d6ebe0e636262170175.jpg" alt=""
+        style={{width:215}} onDoubleClick={handleHeart}/>
+
+        <div className="card-footer">
+        {heart ? <FaHeart style={{fontSize: 50, color: "red"}} onClick={handleHeart}/> : <FaHeart style={{fontSize: 50}} onClick={handleHeart}/>}
+        </div>
+
+        </div>
+    )
+}
+
+export default Card;
